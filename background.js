15c80a41e4b3616856593da8319e329cd971ab10
İbrahim Ekinci <info@ibrahimekinci.dev>
2021-09-06 22:47:51 +0300

    unnecessary codes removed

--- background.js
+++ background.js
@@ -1,8 +1,3 @@
-let color = "#cccf90"
-
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({
-        color
-    });
     console.log("Unfollowers for Instagram started");
 });
